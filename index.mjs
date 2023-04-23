50aca16269fa3e349416c526b5830ecbfd9074b2
divya shankar anand <divya.anand1@oyorooms.com>
2023-04-23 23:18:17 +0530

    added polling

--- index.mjs
+++ index.mjs
@@ -1,12 +1,34 @@
-import fetch from 'node-fetch';
-console.log('starting the project')
-setInterval(() => {
-    const date=new Date()
-    if(date.getHours===10)
-       { 
-        fetch('http://localhost:3000/pushemail')
-        .then(res=>res.json())
-        .then(res=>console.log(res))
-       }
+import fetch from "node-fetch";
+console.log("starting the project");
+setInterval(async () => {
+  const date = new Date();
+  if (date.getHours() === 10) {
+    console.log("starting the polling");
+    let jwt = await fetch("http://localhost:3000/login", {
+      body: JSON.stringify({ username: "sonukumar", password: "sonukumar" }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => (res ? res.jwt : false))
+      .catch((res) => false);
+    console.log(jwt);
 
-}, 1000*60*60);
+    if (jwt) {
+      fetch(
+        `http://localhost:3000/pushemail/` +
+          new URLSearchParams({
+            jwt: jwt,
+          })
+      )
+        .then((res) => {
+          console.log(res.url);
+          return res.json();
+        })
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err));
+    }
+  }
+}, 1000 * 60*60);
